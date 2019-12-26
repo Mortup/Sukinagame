a7c50da7272ad6b63c2c4ecfeed3fe1bc152902a
Mortup <g.uribemontero@gmail.com>
2019-12-26 15:18:42 -0300

    Animacion puerta funciona

---
+++ Assets/Scenes/2_Lab/Clonacion/Clonadora.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clonadora : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("Entro algo");
+        IClonable clonable = collision.gameObject.GetComponent<IClonable>();
+        if (clonable == null)
+            return;
+
+        Debug.Log(clonable.getName());
+    }
+}
