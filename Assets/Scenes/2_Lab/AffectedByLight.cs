62dd3ca3123b0ae4dfed3c98164341e0c2d3880d
Mortup <g.uribemontero@gmail.com>
2019-12-26 20:03:38 -0300

    Se pueden cambiar las luces

---
+++ Assets/Scenes/2_Lab/AffectedByLight.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class AffectedByLight : MonoBehaviour
+{
+    [SerializeField] Sprite lightsOnSprite;
+    [SerializeField] Sprite lightsOffSprite;
+
+    [SerializeField] bool changesAnimator;
+    [SerializeField] RuntimeAnimatorController lightsOnAnimator;
+    [SerializeField] RuntimeAnimatorController lightsOffAnimator;
+
+    private SpriteRenderer sr;
+    private Animator animator;
+
+    private void Awake() {
+        sr = GetComponent<SpriteRenderer>();
+
+        if (changesAnimator)
+            animator = GetComponent<Animator>();
+    }
+
+    public void TurnOnLight() {
+        if (changesAnimator) {
+            float animTime = animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+            animator.runtimeAnimatorController = lightsOnAnimator;
+            animator.Play("DefaultState", 0, animTime);
+        }
+        else {
+            sr.sprite = lightsOnSprite;
+        }
+    }
+
+    public void TurnOffLight() {
+        if (changesAnimator) {
+            float animTime = animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+            animator.runtimeAnimatorController = lightsOffAnimator;
+            animator.Play("DefaultState", 0, animTime);
+        }
+        else {
+            sr.sprite = lightsOffSprite;
+        }
+    }
+}
---
+++ Assets/Scenes/2_Lab/Maquinas/LightManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+    bool lightsOn;
+
+    public void TurnOnLights() {
+        lightsOn = true;
+
+        AffectedByLight[] targets = FindObjectsOfType<AffectedByLight>();
+        for (int i = 0; i < targets.Length; i++) {
+            targets[i].TurnOnLight();
+        }
+    }
+
+    public void TurnOffLights() {
+        lightsOn = false;
+
+        AffectedByLight[] targets = FindObjectsOfType<AffectedByLight>();
+        for (int i = 0; i < targets.Length; i++) {
+            targets[i].TurnOffLight();
+        }
+    }
+
+    public void SwitchLights() {
+        if (lightsOn) {
+            TurnOffLights();
+        }
+        else {
+            TurnOnLights();
+        }
+    }
+
+    private void Start() {
+        TurnOffLights();
+    }
+}
