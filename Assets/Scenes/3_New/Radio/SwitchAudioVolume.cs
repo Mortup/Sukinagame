195c5e6e0a8b57d6d8ae8dd6ccfa5f34950cabb3
Mortup <g.uribemontero@gmail.com>
2020-01-03 19:09:15 -0300

    La radio suena con la cancion creepy

---
+++ Assets/Scenes/3_New/Radio/SwitchAudioVolume.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchAudioVolume : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    private float maxVolume;
+    private float targetVolume = 0f;
+    private bool isOn = false;
+
+    private void Awake() {
+        audioSource = GetComponent<AudioSource>();
+        maxVolume = audioSource.volume;
+    }
+
+    private void Start() {
+        audioSource.volume = targetVolume;
+    }
+
+    private void Update() {
+        audioSource.volume = Mathf.Lerp(audioSource.volume, targetVolume, 0.2f);
+    }
+
+    public void SwitchVolume() {
+        if (isOn) {
+            targetVolume = 0f;
+        }
+        else {
+            targetVolume = maxVolume;
+        }
+
+        isOn = !isOn;
+    }
+}
