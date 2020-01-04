d5a17b09a20ee7a46b2b6a3b8618abe3ec61dd87
Mortup <g.uribemontero@gmail.com>
2020-01-04 20:09:38 -0300

    La silla suena al moverse

---
+++ Assets/Scenes/2_Lab/Silla/SillaAudio.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SillaAudio : MonoBehaviour
+{
+    [SerializeField] AudioSource source;
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        source.Play();
+    }
+}
