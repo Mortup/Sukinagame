b102fd2f6147f28849ce6fc6eceb0b6d38eb0f20
Mortup <g.uribemontero@gmail.com>
2020-01-04 19:26:38 -0300

    Prueba de joystick

---
+++ Assets/Scenes/PruebaJoystick/HorizontalPrueba.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HorizontalPrueba : MonoBehaviour
+{
+    [SerializeField] private Text texto;
+    
+    void Update()
+    {
+        texto.text = "Horizontal Input\n" + Input.GetAxis("Horizontal").ToString();
+    }
+}
