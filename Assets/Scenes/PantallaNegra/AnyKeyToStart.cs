4fa7208df5ae531d896e2d7fb5d3e07a552fa362
Mortup <g.uribemontero@gmail.com>
2020-01-04 19:52:59 -0300

    Pantalla negra al inicio del juego

---
+++ Assets/Scenes/PantallaNegra/AnyKeyToStart.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnyKeyToStart : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.anyKeyDown) {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
