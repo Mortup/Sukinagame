aed77efa3c97cea5d23f5cb4b5900d2f2c2e4147
Mortup <g.uribemontero@gmail.com>
2020-01-04 01:51:44 -0300

    Reinicia el juego al acercarse a leopold

---
+++ Assets/Scenes/3_New/Swirler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Swirler : MonoBehaviour
+{
+    [SerializeField] float swirlSpeed;
+    [SerializeField] Material material;
+
+    float targetSwirl;
+
+    private void Awake() {
+        material = Instantiate(GetComponent<RawImage>().material);
+        GetComponent<RawImage>().material = material;
+    }
+
+    private void Start() {
+        targetSwirl = 0f;
+    }
+
+    public void Swirl() {
+        targetSwirl = 30f;
+    }
+
+    private void Update() {
+        material.SetFloat("_Strength", Mathf.Lerp(material.GetFloat("_Strength"), targetSwirl, swirlSpeed));
+    }
+}
